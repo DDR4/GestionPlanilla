a58e09d53885485f445810fa50488c63322746a0
Enrique Gabriel Velasquez Villavicencio <gabrielx4air@gmail.com>
2021-05-22 13:42:02 -0500

    modificacion 2205 2

--- GP.BusinessLogic/BLBoletaPago.cs
+++ GP.BusinessLogic/BLBoletaPago.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Mime;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 using GP.Common;
 using GP.DataAccess;
 using GP.Entities;
+using iTextSharp.text;
+using iTextSharp.text.html.simpleparser;
+using iTextSharp.text.pdf;
 
 namespace GP.BusinessLogic
 {
@@ -27,28 +34,22 @@ namespace GP.BusinessLogic
 
                 string periodo = obj.HorasTrabajadas.Periodo;
 
-                System.Net.Mail.Attachment attachment;
-                attachment = new System.Net.Mail.Attachment("d:/CVEnriqueVelasquez.docx");
+                byte[] arraybytes = CrearBoletaPago(periodo);
 
-                var urlpantilla = HttpContext.Current.Request.PhysicalApplicationPath + @"Views\Plantillas\PlantillaCorreo.cshtml";
+                string body = CargarPlantilla(periodo);
 
-                string body = System.IO.File.ReadAllText(urlpantilla);
+                string nombrearchivo = "Boleta de Pago " + periodo;
 
-                body = body.Replace("{{periodo}}", periodo);
-
-               
-
-                int i = 1; int? trabajadorid = 0;
-                while (result.Count() <= i)
+                int i = 1; int? trabajadorid = 0; int contador = result.Count();
+                while (contador >= i)
                 {
                     trabajadorid = result.Where(x => x.Indicador == i).Select(y => y.Trabajador.Trabajador_Id).FirstOrDefault();
                     if (trabajadorid > 0)
                     {
-                        EnvioCorreo.Send("gabrielx4air@gmail.com", "Boleta de Pago" + periodo, body, attachment);
+                        EnvioCorreo.Send("gabrielx4air@gmail.com", nombrearchivo, body, arraybytes, nombrearchivo+".pdf");
                     }
                     i++;
                 }
-
 
                 return new Response<IEnumerable<BoletaPago>>(result);
             }
@@ -56,7 +57,91 @@ namespace GP.BusinessLogic
             {
                 return new Response<IEnumerable<BoletaPago>>(ex);
             }
-        }       
+        }
+
+        public byte[] CrearBoletaPago(string periodo)
+        {
+            Document doc = new Document(PageSize.LETTER);
+            byte[] arraybytes = null;
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                PdfWriter writer = PdfWriter.GetInstance(doc, memoryStream);
+                doc.Open();
+                //pdfDoc = CrearBoletaPago(periodo);
+                string titulo = "Boleta de Pago " + periodo;
+
+                doc.AddTitle(titulo);
+                doc.AddCreator("Enrique Velasquez");
+
+                // Abrimos el archivo
+                doc.Open();
+
+                Font standardFont = new Font(Font.FontFamily.HELVETICA, 8, Font.NORMAL, BaseColor.BLACK);
+
+                // Escribimos el encabezamiento en el documento
+                doc.Add(new Paragraph(titulo));
+                doc.Add(Chunk.NEWLINE);
+
+                // Creamos una tabla que contendrá el nombre, apellido y país
+                // de nuestros visitante.
+                PdfPTable tblPrueba = new PdfPTable(3);
+                tblPrueba.WidthPercentage = 100;
+
+                // Configuramos el título de las columnas de la tabla
+                PdfPCell clNombre = new PdfPCell(new Phrase("Nombre", standardFont));
+                clNombre.BorderWidth = 1;
+                clNombre.BorderWidthBottom = 0.75f;
+
+                PdfPCell clApellido = new PdfPCell(new Phrase("Apellido", standardFont));
+                clApellido.BorderWidth = 1;
+                clApellido.BorderWidthBottom = 0.75f;
+
+                PdfPCell clPais = new PdfPCell(new Phrase("País", standardFont));
+                clPais.BorderWidth = 1;
+                clPais.BorderWidthBottom = 0.75f;
+
+                // Añadimos las celdas a la tabla
+                tblPrueba.AddCell(clNombre);
+                tblPrueba.AddCell(clApellido);
+                tblPrueba.AddCell(clPais);
+
+                // Llenamos la tabla con información
+                clNombre = new PdfPCell(new Phrase("Roberto", standardFont));
+                clNombre.BorderWidth = 1;
+
+                clApellido = new PdfPCell(new Phrase("Torres", standardFont));
+                clApellido.BorderWidth = 1;
+
+                clPais = new PdfPCell(new Phrase("Puerto Rico", standardFont));
+                clPais.BorderWidth = 1;
+
+                // Añadimos las celdas a la tabla
+                tblPrueba.AddCell(clNombre);
+                tblPrueba.AddCell(clApellido);
+                tblPrueba.AddCell(clPais);
+
+                doc.Add(tblPrueba);
+
+                doc.Close();
+                arraybytes = memoryStream.ToArray();
+                memoryStream.Close();
+            }
+
+            return arraybytes;
+
+        }
+
+
+        public string CargarPlantilla(string periodo)
+        {
+            var urlpantilla = HttpContext.Current.Request.PhysicalApplicationPath + @"Views\Plantillas\PlantillaCorreo.cshtml";
+
+            string body = File.ReadAllText(urlpantilla);
+
+            body = body.Replace("{{periodo}}", periodo);
+
+            return body;
+        }
 
     }
 }
--- GP.Common/EnvioCorreo.cs
+++ GP.Common/EnvioCorreo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
@@ -10,27 +11,34 @@ namespace GP.Common
 {
     public class EnvioCorreo
     {
-        public static void Send(string destino,string asunto,string body,Attachment attachment)
+        public static void Send(string destino,string asunto,string body,byte[] arraybytes,string nombrearchivo)
         {
-            MailMessage mail = new MailMessage();
-            SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
 
-            var email = EncriptacionBase64.Base64Decode(ConfigurationUtilities.GetAppSettings("Email"));
+                var email = EncriptacionBase64.Base64Decode(ConfigurationUtilities.GetAppSettings("Email"));
 
-            mail.From = new MailAddress(destino);
-            mail.To.Add(email);
-            mail.Subject = asunto;
-            mail.Body = body;
+                mail.From = new MailAddress(destino);
+                mail.To.Add(email);
+                mail.Subject = asunto;
+                mail.Body = body;
 
-            mail.Attachments.Add(attachment);
+                mail.Attachments.Add(new Attachment(new MemoryStream(arraybytes), nombrearchivo));
        
-            var clave = EncriptacionBase64.Base64Decode(ConfigurationUtilities.GetAppSettings("Clave"));
+                var clave = EncriptacionBase64.Base64Decode(ConfigurationUtilities.GetAppSettings("Clave"));
 
-            SmtpServer.Port = 587;
-            SmtpServer.Credentials = new System.Net.NetworkCredential(email, clave);
-            SmtpServer.EnableSsl = true;
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential(email, clave);
+                SmtpServer.EnableSsl = true;
 
-            SmtpServer.Send(mail);
+                SmtpServer.Send(mail);
+            }
+            catch (Exception ex)
+            {
+                ex.Message.ToString();
+            }
 
         }
 
