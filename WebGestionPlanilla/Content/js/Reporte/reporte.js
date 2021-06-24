43ec1ed5f3d5ccc3f2e26b5a778042a068efa805
Enrique Gabriel Velasquez Villavicencio <gabrielx4air@gmail.com>
2021-06-23 22:30:08 -0500

    e modificacion 2306 2

--- WebGestionPlanilla/Content/js/Reporte/reporte.js
+++ WebGestionPlanilla/Content/js/Reporte/reporte.js
@@ -2,7 +2,7 @@
 
     var $txtFechaReporte = $('#txtFechaReporte');
     var $btnGenerarReporteTurno = $('#btnGenerarReporteTurno');
-
+    var $btnGenerarReporteArea = $('#btnGenerarReporteArea');    
                                         
     var Message = {
         ObtenerTipoBusqueda: "Obteniendo los tipos de busqueda, Por favor espere...",
@@ -22,12 +22,40 @@
             minViewMode: "months"
         });      
         $btnGenerarReporteTurno.click($btnGenerarReporteTurno_click);
+        $btnGenerarReporteArea.click($btnGenerarReporteArea_click);
     }           
 
     function $btnGenerarReporteTurno_click() {
+        if (ValidaGenerarReporte()) {
+            GenerarReporteTurno();
+        }            
+    }
+
+    function GenerarReporteTurno() {
         app.RedirectTo("Reporte/GenerarReporteTurno?Periodo=" + $txtFechaReporte.val());
     }
 
+    function $btnGenerarReporteArea_click() {
+        if (ValidaGenerarReporte()) {
+            GenerarReporteArea();
+        }
+    }
+
+    function GenerarReporteArea() {
+        app.RedirectTo("Reporte/GenerarReporteArea?Periodo=" + $txtFechaReporte.val());
+    }    
+
+    function ValidaGenerarReporte() {
+        var val = false;
+
+        if ($txtFechaReporte.val() !== "") {
+            val = true;
+        } else {
+            app.Message.Info("Aviso", "Seleccionar la fecha del Reporte", "Aceptar", null);
+        }
+
+        return val;
+    }
 
 
 })(window.jQuery, window, document);
--- WebGestionPlanilla/Controllers/ReporteController.cs
+++ WebGestionPlanilla/Controllers/ReporteController.cs
@@ -101,6 +101,90 @@ namespace WebGestionPlanilla.Controllers
             Response.End();
         }
 
+        public void GenerarReporteArea(string Periodo)
+        {
+            var NombreExcel = "Reporte Turno";
+
+            // Recuperamos la data  de las consulta DB
+            var bussingLogic = new GP.BusinessLogic.BLReporte();
+            var data = bussingLogic.GetReporteTurno(Periodo);
+
+            string[] Cabezeras = {
+                    "Nombre y Apellido", "Turno", "Asistencia", "Tardanzas", "Faltas"
+            };
+
+            // Creación del libro excel xlsx.
+            var wb = new XSSFWorkbook();
+
+            // Creación del la hoja y se especifica un nombre
+            ISheet sheet = wb.CreateSheet(NombreExcel);
+
+            // Contadores para filas y columnas.
+            int rownum = 0;
+            int cellnum = 0;
+
+            // Creacion del estilo de la letra para las cabeceras.
+            var fontCab = wb.CreateFont();
+            fontCab.FontHeightInPoints = 12;
+            fontCab.FontName = "Calibri";
+            fontCab.Color = HSSFColor.White.Index;
+
+            // Creacion del color del estilo.
+            var colorCab = new XSSFColor(new byte[] { 7, 105, 173 });
+
+            // Se crea el estilo y se agrega el font al estilo
+            var styleCab = (XSSFCellStyle)wb.CreateCellStyle();
+            styleCab.SetFont(fontCab);
+            styleCab.FillForegroundXSSFColor = colorCab;
+            styleCab.FillPattern = FillPattern.SolidForeground;
+
+            // Se crea la primera fila para las cabceras.
+            IRow row = sheet.CreateRow(rownum++);
+            ICell cell;
+
+            foreach (var item in Cabezeras)
+            {
+                // Se crea celdas y se agrega las cabeceras
+                cell = row.CreateCell(cellnum++);
+                cell.SetCellValue(item);
+                cell.CellStyle = styleCab;
+
+                sheet.AutoSizeColumn(cellnum);
+            }
+
+            // Creacion del estilo de la letra para la data.
+            var fontBody = wb.CreateFont();
+            fontBody.FontHeightInPoints = 12;
+            fontBody.FontName = "Arial";
+
+            var styleBody = (XSSFCellStyle)wb.CreateCellStyle();
+            styleBody.SetFont(fontBody);
+
+
+            // Impresión de la data
+            foreach (var item in data.Data)
+            {
+                cellnum = 0;
+                row = sheet.CreateRow(rownum++);
+
+                sheet.AutoSizeColumn(cellnum);
+                AddValue(row, cellnum++, item.Nombres.ToString(), styleBody, sheet);
+                AddValue(row, cellnum++, item.Turno.Descripcion.ToString(), styleBody, sheet);
+                AddValue(row, cellnum++, item.HorasTrabajadas.DiasTrabajados.ToString(), styleBody, sheet);
+                AddValue(row, cellnum++, item.HorasTrabajadas.DiasTardanzas.ToString(), styleBody, sheet);
+                AddValue(row, cellnum++, item.HorasTrabajadas.DiasNoTrabajados.ToString(), styleBody, sheet);
+            }
+
+
+            var nameFile = NombreExcel + "_" + DateTime.Now.ToString("dd_MM_yyyy HH:mm:ss") + ".xlsx";
+            Response.AddHeader("content-disposition", "attachment; filename=" + nameFile);
+            Response.ContentType = "application/octet-stream";
+            Stream outStream = Response.OutputStream;
+            wb.Write(outStream);
+            outStream.Close();
+            Response.End();
+        }
+
         public void AddValue(IRow row, int cellnum, string value, ICellStyle styleBody, ISheet sheet)
         {
             ICell cell;
